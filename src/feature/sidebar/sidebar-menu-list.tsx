9edfd26ee59c0e577eb647f4fddfc629546104c4
hyeopsang <dbtkdguq3@gmail.com>
2025-11-07 16:36:42 +0900

    style(side-bar): 사이드 바 다크 모드 적용 및 스타일 변경

--- src/feature/sidebar/sidebar-menu-list.tsx
+++ src/feature/sidebar/sidebar-menu-list.tsx
@@ -3,15 +3,26 @@ import Dashboard from '@/assets/dashboard.svg?react';
 import History from '@/assets/history.svg?react';
 import Setting from '@/assets/setting.svg?react';
 
+const menuItems = [
+  {label: '대시보드', icon: Dashboard, path: '/my-app/dashboard'},
+  {label: '히스토리', icon: History, path: '/my-app/history'},
+  {label: '설정', icon: Setting, path: '/my-app/setting'},
+];
+
 export default function SideBarMenuList() {
   return (
     <nav
       className="flex flex-col w-full flex-1 py-5 gap-2"
       aria-label="사이드바 메뉴"
     >
-      <MenuButton label="대시보드" icon={Dashboard} path="/my-app/dashboard" />
-      <MenuButton label="히스토리" icon={History} path="/my-app/history" />
-      <MenuButton label="설정" icon={Setting} path="/my-app/setting" />
+      {menuItems.map((item) => (
+        <MenuButton
+          key={item.path}
+          label={item.label}
+          icon={item.icon}
+          path={item.path}
+        />
+      ))}
     </nav>
   );
 }
